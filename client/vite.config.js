3e749aa341e24bba29a046459b6021c8fabcadc1
alexmam80 <166691364+alexmam80@users.noreply.github.com>
2025-08-14 17:28:09 +0300

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// BASE este necesar pentru GitHub Pages când site-ul nu e pe domeniu root
-const base = process.env.GITHUB_PAGES === 'true' ? (process.env.BASE_PATH || '/') : '/'
-
 export default defineConfig({
   plugins: [react()],
-  base,
+  base: '/SF/',   // <- numele repo-ului tău
 })
