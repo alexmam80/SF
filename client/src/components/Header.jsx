d234140ac874cd879ffb1ed90af3b723f3b70729
alexmam80 <166691364+alexmam80@users.noreply.github.com>
2025-08-19 10:13:06 +0300

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,13 +2,18 @@ import { NavLink } from 'react-router-dom'
 import { FiPhoneCall, FiInstagram, FiMail } from 'react-icons/fi'
 import { useTranslation } from 'react-i18next'
 import LanguageSwitcher from './LanguageSwitcher'
+import logo from '../assets/gallery/logo.png'
 
 export default function Header() {
   const { t } = useTranslation()
   return (
     <header className="site-header sticky">
       <div className="container header-inner">
-        <div className="logo">MyLogo</div>
+        <div className="logo">
+          <a href="#/" className="logo-a" aria-label="Ultraservice SRL">
+            <img src={logo} alt="Salon de Frumusețe" className="logo-img" />
+          </a>
+        </div>
         <nav className="main-nav">
           <NavLink to="/" end>{t('nav.home')}</NavLink>
           <NavLink to="/about">{t('nav.about')}</NavLink>
